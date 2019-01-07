429b36e1de0be69d66dd1968fb151521a7153986
srect <fcqun@sina.cn>
2019-01-07 23:26:55 +0800

    :construction: feat: Container组件

--- lib/main.dart
+++ lib/main.dart
@@ -18,9 +18,24 @@ class MyApp extends StatelessWidget {
               ),
             ),
             alignment: Alignment.bottomCenter,
-            width: 500,
-            height: 300,
-            color: Colors.blue,
+            width: 300,
+            height: 200,
+            // color: Colors.blue,
+            // padding: const EdgeInsets.all(50.0), // 内边距上下左右全为10
+            padding: const EdgeInsets.fromLTRB(40.0, 50.0, 10.0, 20.0),
+            margin: const EdgeInsets.all(20),
+            // 如果设置了decoration属性，就要去除上面的color属性
+            decoration: new BoxDecoration(
+              // 背景色渐变
+              gradient: const LinearGradient(
+                colors: [Colors.yellowAccent, Colors.red],
+              ),
+              // 边框
+              border: Border.all(
+                width: 2.0,
+                color: Colors.black87,
+              ),
+            ),
           ),
         ),
       ),
