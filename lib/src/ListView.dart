f9f164e2978f7cc12ec0f9b77600eeb52d80dfa7
srect <fcqun@sina.cn>
2019-01-15 23:06:12 +0800

    :construction: feat: 列表横向滚动

--- lib/src/ListView.dart
+++ lib/src/ListView.dart
@@ -34,12 +34,49 @@ class MyListView extends StatelessWidget {
                   fit: BoxFit.contain,
                 ),
                 width: 100.0,
-                height: 200.0,
+                height: 300.0,
                 decoration: new BoxDecoration(
                   border: Border.all(
                     width: 2.0,
                     color: Colors.purple,
                   ),
+                ),
+              ),
+              new Container(
+                height: 80.0,
+                margin: EdgeInsets.fromLTRB(0, 20.0, 0, 20.0),
+                decoration: new BoxDecoration(
+                  border: new Border(
+                    top: new BorderSide(width: 2.0, color: Colors.lightBlue),
+                    right: new BorderSide(width: 2.0, color: Colors.tealAccent),
+                    bottom: new BorderSide(width: 2.0, color: Colors.lightBlue),
+                    left: new BorderSide(width: 2.0, color: Colors.tealAccent),
+                  ),
+                ),
+                child: new ListView(
+                  scrollDirection: Axis.horizontal, // 横向滚动
+                  children: <Widget>[
+                    new Container(
+                      width: 130.0,
+                      color: Colors.yellow,
+                      child: new Text('1'),
+                    ),
+                    new Container(
+                      width: 110.0,
+                      color: Colors.greenAccent,
+                      child: new Text('2'),
+                    ),
+                    new Container(
+                      width: 120.0,
+                      color: Colors.blueGrey,
+                      child: new Text('3'),
+                    ),
+                    new Container(
+                      width: 110.0,
+                      color: Colors.orange,
+                      child: new Text('4'),
+                    ),
+                  ],
                 ),
               ),
             ],
